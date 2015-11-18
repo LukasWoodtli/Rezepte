8fa4c771e191f8faef3f23277b7d011845bf5f47
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-11-18 20:27:23 +0100

    Add my own clone of blue-penguin theme

--- clone_theme.sh
+++ clone_theme.sh
@@ -1,3 +1,3 @@
 mkdir ~/blue-penguin
-git clone --depth 1 https://github.com/jody-frankowski/blue-penguin.git ~/blue-penguin
+git clone --depth 1 https://github.com/LukasWoodtli/blue-penguin.git ~/blue-penguin
 pelican-themes -i ~/blue-penguin
