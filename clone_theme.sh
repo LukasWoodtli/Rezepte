ba7cb6cee6a9b14681e266bb345e4c5b631acfd7
Lukas Woodtli (Travis CI) <woodtli.lukas@gmail.com>
2025-01-06 16:07:40 +0100

    Small fixes

--- clone_theme.sh
+++ clone_theme.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 mkdir ~/blue-penguin
-git clone --depth 1 https://github.com/LukasWoodtli/blue-penguin.git ~/blue-penguin
-pelican-themes -i ~/blue-penguin
+rm -rf /tmp/blue-penguin
+git clone --depth 1 https://github.com/LukasWoodtli/blue-penguin.git /tmp/blue-penguin
+pelican-themes -i /tmp/blue-penguin
